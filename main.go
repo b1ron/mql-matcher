dc968e15a5b9988b34d6d0692cc6fc9ca12c21e3
b1ron <goldingbyron@gmail.com>
2023-06-08 00:32:21 +0200

    stuff

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ type Expr interface {
 
 type tree struct {
 	node *node
+	// ...
+	left  *node
+	right *node
 }
 
 type node struct {
@@ -31,28 +34,64 @@ type leaf struct {
 // returns a leaf value.
 func (l *leaf) eval() int {
 	switch l.value.(type) {
-	case *node:
-		return l.value.(*node).expr.eval()
 	case []any:
+		// TODO iterate the slice
 		return len(l.value.([]any))
+	case int:
+		return l.value.(int)
 	}
 	return -1
 }
 
+// evaluates a node expression
+func (n *node) eval() int {
+	switch n.expr.(type) {
+	case *node:
+		return n.expr.eval()
+	case *leaf:
+		return n.expr.eval()
+	}
+	return -1
+}
+
+// evaluates an expression tree
+func (t *tree) eval() int {
+	switch t.node.expr.(type) {
+	case *node:
+		return t.node.expr.eval()
+	case *leaf:
+		return t.node.expr.eval()
+	}
+	return -1
+}
+
+func traverse(t *tree) {
+	// TODO
+}
+
 func main() {
 	// build a tree of simple nested expressions
-	t := &tree{
+	t1 := &tree{
+		node: &node{
+			expr: &leaf{
+				key:   "foo",
+				value: 1,
+			},
+		},
+	}
+
+	t2 := &tree{
 		node: &node{
 			expr: &leaf{
 				key: "foo",
-				value: &node{
-					expr: &leaf{
-						key:   "bar",
-						value: []any{1, 2, 3},
-					},
+				value: []any{
+					[]any{1, 2, 3},
 				},
 			},
 		},
 	}
-	fmt.Println(t.node.expr.eval())
+	// traverses the expression tree post-order and compares each leaf node
+	// if A is a subset of B, then A EQ B.
+	tree := &tree{left: t1.node, right: t2.node}
+	fmt.Println(tree.eval())
 }
