1e58baa595f151e4c15ac5a8020a3767d3a94cd8
b1ron <goldingbyron@gmail.com>
2023-06-09 21:51:39 +0200

    more

--- ast/ast.go
+++ ast/ast.go
@@ -62,9 +62,22 @@ func (t *tree) eval() any {
 
 func isSubset(a, b *tree) bool {
 	lhs := a.eval()
-	switch {
-	case lhs == b.eval():
-		return true
+	switch lhs.(type) {
+	case int:
+		switch b.eval().(type) {
+		case int:
+			return lhs.(int) == b.eval().(int)
+		case []any:
+			return contains(b.eval().([]any), lhs.(int))
+		}
 	}
+	return false
+}
+
+type number interface {
+	int | float64 | float32 | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
+}
+
+func contains[K comparable, V number]([]any, any) bool {
 	return false
 }
